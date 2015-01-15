d91eb6c553073c2b84948bd9be7bb7539bfbfc20
EvgenyT <evgeny.tukin@9rays.net>
2015-01-15 15:45:50 +0300

    Cleanup

--- src/Pranas.ScreenshotCapture.Test/Program.cs
+++ src/Pranas.ScreenshotCapture.Test/Program.cs
@@ -2,13 +2,13 @@
 using System.Drawing.Imaging;
 using System.IO;
 
-namespace Pranas.ScreenshotCapture.Test
+namespace Pranas.Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-	        Console.WriteLine("Taking screenshot... OS : {0} ", (int) Environment.OSVersion.Platform);
+	        Console.WriteLine("Taking screenshot... OS : {0} ",  Environment.OSVersion.Platform);
 			
 	        var screen = ScreenshotCapture.TakeScreenshot();
 	        screen.Save(Path.Combine(Environment.CurrentDirectory, "Screenshot.jpg"), ImageFormat.Jpeg);
--- src/Pranas.ScreenshotCapture/ScreenshotCapture.cs
+++ src/Pranas.ScreenshotCapture/ScreenshotCapture.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Copyright © ScreenshotMonitor 2015
+// http://screenshotmonitor.com/
+// 
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
@@ -7,52 +13,40 @@ using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace Pranas.ScreenshotCapture
+namespace Pranas
 {
     /// <summary>
-    /// ScreenshotCapture
+    ///     ScreenshotCapture
     /// </summary>
     public static class ScreenshotCapture
     {
         #region Public static methods
 
         /// <summary>
-        /// Capture screen to bytes array
+        ///     Capture screenshot to Image object
         /// </summary>
         /// <param name="onlyPrimaryScreen">Create screen only from primary screen</param>
         /// <returns></returns>
-        public static Image TakeScreenshot(bool onlyPrimaryScreen = true)
+        public static Image TakeScreenshot(bool onlyPrimaryScreen = false)
         {
             try
             {
-               return WindowsCapture(onlyPrimaryScreen);
+                return WindowsCapture(onlyPrimaryScreen);
             }
             catch (Exception)
             {
-				return OsXCapture(onlyPrimaryScreen);
+                return OsXCapture(onlyPrimaryScreen);
             }
-            
-            //switch (OsDetector.DetectOs())
-            //{
-            //    case OsType.Windows:
-            //    case OsType.OtherUnix:
-            //        return WindowsCapture(onlyPrimaryScreen);
-            //    case OsType.OsX:
-            //        return OsXCapture(onlyPrimaryScreen);
-            //    //case OsType.Linux:
-            //    //    return LinuxCapture(onlyPrimaryScreen);
-            //}
         }
 
         #endregion
 
         #region  Private static methods
 
-
-		//private static Image ImageMagicCapture(bool onlyPrimaryScreen)
-		//{
-		//	return ExecuteCaptureProcess("import", "-window root ");
-		//}
+        //private static Image ImageMagicCapture(bool onlyPrimaryScreen)
+        //{
+        //	return ExecuteCaptureProcess("import", "-window root ");
+        //}
 
         private static Image OsXCapture(bool onlyPrimaryScreen)
         {
@@ -63,20 +57,20 @@ namespace Pranas.ScreenshotCapture
         }
 
 
-	    /// <summary>
-	    /// Start execute process with parameters	
-	    /// </summary>
-	    /// <param name="execModule">Application name</param>
-	    /// <param name="parameters">Command line parameters</param>
-	    /// <returns>Bytes for destination image</returns>
-	    private static Image ExecuteCaptureProcess(string execModule, string parameters)
+        /// <summary>
+        ///     Start execute process with parameters
+        /// </summary>
+        /// <param name="execModule">Application name</param>
+        /// <param name="parameters">Command line parameters</param>
+        /// <returns>Bytes for destination image</returns>
+        private static Image ExecuteCaptureProcess(string execModule, string parameters)
         {
             var imageFileName = Path.Combine(Path.GetTempPath(), string.Format("screenshot_{0}.jpg", Guid.NewGuid()));
 
             var process = Process.Start(execModule, string.Format("{0} {1}", parameters, imageFileName));
             if (process == null)
             {
-                throw new InvalidOperationException("Executable of '" + execModule + "' was not found");
+                throw new InvalidOperationException(string.Format("Executable of '{0}' was not found", execModule));
             }
             process.WaitForExit();
 
@@ -87,7 +81,7 @@ namespace Pranas.ScreenshotCapture
 
             try
             {
-				return Image.FromFile(imageFileName);
+                return Image.FromFile(imageFileName);
             }
             finally
             {
@@ -95,46 +89,20 @@ namespace Pranas.ScreenshotCapture
             }
         }
 
-        ///// <summary>
-        ///// ChangeImage quality
-        ///// </summary>
-        ///// <param name="fileBytes">Source image bytes</param>
-        ///// <returns>Changed image bytes</returns>
-        //private static byte[] ChangeJpegQuality(byte[] fileBytes)
-        //{
-        //    const int MaxHeight = 1600;
+        /// <summary>
+        ///     Capture screenshot with .NET standard implementation
+        /// </summary>
+        /// <param name="onlyPrimaryScreen"></param>
+        /// <returns>Return bytes of screenshot image</returns>
+        private static Image WindowsCapture(bool onlyPrimaryScreen)
+        {
+            if (onlyPrimaryScreen) return ScreenCapture(Screen.PrimaryScreen);
+            var bitmaps = (Screen.AllScreens.OrderBy(s => s.Bounds.Left).Select(ScreenCapture)).ToArray();
+            return CombineBitmap(bitmaps);
+        }
 
-        //    using (var bitmap = Image.FromStream(new MemoryStream(fileBytes)))
-        //    {
-        //        var q = (bitmap.Height < MaxHeight) ? 40L : 4L;
-        //        using (var stream = new MemoryStream())
-        //        {
-        //            var codecs = ImageCodecInfo.GetImageDecoders();
-        //            var jgpEncoder = codecs.First(codec => codec.FormatID == ImageFormat.Jpeg.Guid);
-        //            var encoderParams = new EncoderParameters(1);
-        //            var qualityParam = new EncoderParameter(Encoder.Quality, q);
-        //            encoderParams.Param[0] = qualityParam;
-        //            bitmap.Save(stream, jgpEncoder, encoderParams);
-
-        //            return stream.ToArray();
-        //        }
-        //    }
-        //}
-
-	    /// <summary>
-	    /// Capture screenshots with .NET standard features
-	    /// </summary>
-	    /// <param name="onlyPrimaryScreen"></param>
-	    /// <returns>Return bytes of screenshot image</returns>
-	    private static Image WindowsCapture(bool onlyPrimaryScreen)
-	    {
-		    if (onlyPrimaryScreen) return ScreenCapture(Screen.PrimaryScreen);
-		    var bitmaps = (Screen.AllScreens.OrderBy(s => s.Bounds.Left).Select(ScreenCapture)).ToArray();
-		    return CombineBitmap(bitmaps);
-	    }
-
-	    /// <summary>
-        /// Create screenshot of one screen
+        /// <summary>
+        ///     Create screenshot of single display
         /// </summary>
         /// <param name="screen"></param>
         /// <returns></returns>
@@ -157,10 +125,10 @@ namespace Pranas.ScreenshotCapture
         }
 
         /// <summary>
-        /// Combime image collection in one
+        ///     Combime images collection in one bitmap
         /// </summary>
         /// <param name="images"></param>
-        /// <returns>Bytes of destination Bitmap image</returns>
+        /// <returns>Combined image</returns>
         private static Image CombineBitmap(ICollection<Image> images)
         {
             Image finalImage = null;
@@ -186,7 +154,7 @@ namespace Pranas.ScreenshotCapture
                     foreach (var image in images)
                     {
                         g.DrawImage(image,
-                          new Rectangle(offset, 0, image.Width, image.Height));
+                            new Rectangle(offset, 0, image.Width, image.Height));
                         offset += image.Width;
                     }
                 }
